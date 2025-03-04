9055c3bdddbd019cff49933082daf77382d96f9b
Matt <matt6gsr@gmail.com>
2025-03-04 16:45:57 +0000

    added hover to darkmode button

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,9 +3,9 @@ import { HeaderProps } from "../types";
 
 const Header: React.FC<HeaderProps> = ({ isDarkMode, toggleDarkMode }) => {
   return (
-    <header className="p-4 flex justify-between items-center shrink-0">
+    <header className="p-4 flex justify-between items-center shrink-0 bg-gray-100 dark:bg-gray-900 transition-colors duration-300 gap-2">
       <h1 className="text-xl sm:text-2xl font-bold text-gray-800 dark:text-white">
-        Task Manager
+        Oak Tyres Task Manager
       </h1>
       <button
         onClick={toggleDarkMode}
