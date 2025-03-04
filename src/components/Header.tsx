88c380862f83d9dd3c3e25f1ffc038f6fe3b6641
Matt <matt6gsr@gmail.com>
2025-03-04 16:51:06 +0000

    remove

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,7 +3,7 @@ import { HeaderProps } from "../types";
 
 const Header: React.FC<HeaderProps> = ({ isDarkMode, toggleDarkMode }) => {
   return (
-    <header className="p-4 flex justify-between items-center shrink-0 bg-gray-100 dark:bg-gray-900 transition-colors duration-300 gap-2">
+    <header className="p-4 flex justify-between items-center shrink-0 ">
       <h1 className="text-xl sm:text-2xl font-bold text-gray-800 dark:text-white">
         Oak Tyres Task Manager
       </h1>
